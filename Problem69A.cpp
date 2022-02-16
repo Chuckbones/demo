d858864515cc64eeb0a7a64cd8624b0c0b3caac1
Chuckbones <shivamojha42@gmail.com>
2022-02-16 14:06:54 +0530

    new file on new branch

---
+++ Problem69A.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void hello();
+int main()
+{
+        int a,b,c,d,e,f,g,n,x;
+        cin>>n;
+        ;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a>>b>>c;
+            d=a+b+c;
+            
+        }
+        cout<<d;
+
+return 0;
+}
+void hello()
+{
+        
+}
